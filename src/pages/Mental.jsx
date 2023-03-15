39483b6f3697cc6ffe1f4ba5d3bee86f884d7837
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2023-03-15 16:23:41 -0300

    Create Mental.jsx

---
+++ src/pages/Mental.jsx
@@ -0,0 +1,26 @@
+import './Mentalstyle.css';
+
+const ML = () => {
+  return (
+    <div className="Geral" >
+        <p className="p1" >
+          <strong>MUNDO, LUZ E CORES</strong>
+        </p>
+        <p>Um exercício para o capítulo: A luz, as cores e os nossos olhos. </p>
+        <p>Como usar:</p>
+        <p>Nos seus celulares entrem na página kahoot.it e em seguida digitem o código indicado na página pricipal do professor.</p>
+        <div>
+        <img src="https://images.squarespace-cdn.com/content/v1/5eee731ceaa57e78d7b3f47f/1593151412879-KOO6VAVW9WHL6P2IZUWM/1.jpg?format=1000w" alt="Kahoot" height={"539px"}/>
+        </div>
+        <p>Desmos:</p>
+        <p>Um aaplicação similar ao GeoGebra, Symbolab e outros do gênero,  é uma calculadora gráfica avançada implementada como um aplicativo da web e um aplicativo móvel escrito em JavaScript. O nome Desmos veio da palavra grega δεσμός , que significa vínculo, "relação". </p>
+        <p>Demonstração:</p>
+        <p>Uma curiosidade que chama a atenção para as possibilidades.</p>
+        <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/BQvBq3K50u8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <p className="p1"><strong>Prática de Ensino</strong></p>
+    </div>
+  );
+}
+export default Mental;
